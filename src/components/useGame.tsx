d9bf5fdfff5bb8dba4898c0853948dac40c4c1ab
opa97 <opa9788@gmail.com>
2024-06-04 16:28:13 +0900

    一番したに行ったら動かなくなった

--- src/components/useGame.tsx
+++ src/components/useGame.tsx
@@ -1,11 +1,6 @@
-import React from 'react';
 import { useState } from 'react';
 
 const useGame = () => {
-  // ArrowLeft
-  // ArrowDown
-  //  ArrowRigh
-
   const [board, setBoard] = useState([
     [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -26,20 +21,37 @@ const useGame = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 2, 0, 0, 0],
   ]);
 
-  const stopMovingCell = (rowIndex: number, cellIndex: number, movingCell: number[][]) => {
+
+
+  const flingOutOfBoard = (rowIndex: number, cellIndex: number, movingCell: number[][]) => {
     for (let i = 0; i < movingCell.length; i++) {
       const extendRowIndex = movingCell[i][0] + rowIndex;
       const extendCellIndex = movingCell[i][1] + cellIndex;
-      console.log(extendRowIndex, extendCellIndex);
+
       if (extendCellIndex < 0 || extendCellIndex > 9 || extendRowIndex < 0 || extendRowIndex > 19) {
         return true;
       }
     }
 
     return false;
+  };
+
+  const stopCell = (movingCell: number[][]) => {
+    const newBoard = structuredClone(board)
+    const newMovingCell = structuredClone(movingCell)
+    newMovingCell.map((row:number[])=>{
+      if(row[0] + 1 > 19 || newBoard[row[0] + 1][row[1]] === 2){
+        newMovingCell.map((row:number[])=>{
+          newBoard[row[0]][row[1]] = 2
+        })
+      }
+    })
+     return newBoard
+
+
   };
 
   const findMovingCell = () => {
@@ -52,7 +64,6 @@ const useGame = () => {
         }
       });
     });
-    console.log(movingCell);
 
     return movingCell;
   };
@@ -78,22 +89,42 @@ const useGame = () => {
 
     if (keyValue === 'ArrowDown') {
       const movingCell = findMovingCell();
-      if (!stopMovingCell(1, 0, movingCell)) {
+      if(stopCell(movingCell).flat().some(cell => cell === 2)){
+        const newBoard = stopCell(movingCell)
+        setBoard(newBoard)
+      }
+
+      if (!flingOutOfBoard(1, 0, movingCell)) {
         const newBoard = moveCell(1, 0, movingCell);
         setBoard(newBoard);
       }
     } else if (keyValue === 'ArrowLeft') {
       const movingCell = findMovingCell();
-      if (!stopMovingCell(0, -1, movingCell)) {
+      if (!flingOutOfBoard(0, -1, movingCell)) {
         const newBoard = moveCell(0, -1, movingCell);
         setBoard(newBoard);
       }
     } else if (keyValue === 'ArrowRight') {
       const movingCell = findMovingCell();
-      if (!stopMovingCell(0, 1, movingCell)) {
+      if (!flingOutOfBoard(0, 1, movingCell)) {
         const newBoard = moveCell(0, 1, movingCell);
         setBoard(newBoard);
       }
+    } else if (keyValue === 'Space') {
+      const movingCell = findMovingCell();
+      const newBoard = structuredClone(board);
+      movingCell.map((row) => {
+        newBoard[row[0]][row[1]] = 0;
+      });
+      while (!flingOutOfBoard(1, 0, movingCell)) {
+        movingCell.map((row) => {
+          row[0] += 1;
+        });
+      }
+      movingCell.map((row) => {
+        newBoard[row[0]][row[1]] = 2;
+      });
+      setBoard(newBoard);
     }
   };
 
