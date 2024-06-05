b8c3d6eedbc9352ab7550a53ab044b2f533f2f3c
opa97 <opa9788@gmail.com>
2024-06-05 11:42:40 +0900

    関数を別ファイルに分けた

--- src/components/useGame.tsx
+++ src/components/useGame.tsx
@@ -1,40 +1,11 @@
 import { useEffect, useState } from 'react';
-
-const findMovingCell = (board: number[][]) => {
-  const movingCell: number[][] = [];
-
-  board.map((row: number[], rowIndex: number) => {
-    row.map((cell, cellIndex: number) => {
-      if (cell === 1) {
-        movingCell.push([rowIndex, cellIndex]);
-      }
-    });
-  });
-
-  return movingCell;
-};
-
-const moveCell = (
-  rowIndex: number,
-  cellIndex: number,
-  movingCell: number[][],
-  board: number[][],
-) => {
-  const newBoard = structuredClone(board);
-  console.log(rowIndex);
-  movingCell.map(([row, cell]: number[]) => {
-    newBoard[row][cell] = 0;
-  });
-
-  movingCell.map(([row, cell]: number[]) => {
-    const extendRowIndex = row + rowIndex;
-    const extendCellIndex = cell + cellIndex;
-
-    newBoard[extendRowIndex][extendCellIndex] = 1;
-  });
-
-  return newBoard;
-};
+import {
+  findMovingCell,
+  moveCell,
+  flingOutOfBoard,
+  checkUnderCell,
+  stopCell,
+} from '../functions/functions';
 
 const useGame = () => {
   const [board, setBoard] = useState([
@@ -62,13 +33,12 @@ const useGame = () => {
 
   const [boardUpdated, setBoardUpdate] = useState(false);
 
+  //そこにブロックが到達したときに、止める処理
   useEffect(() => {
     const movingCell = findMovingCell(board);
-    const newBoard = stopCell(movingCell);
+    const newBoard = stopCell(movingCell, board);
     setBoard(newBoard);
   }, [boardUpdated]);
-
-
 
   const autoDropCell = () => {
     setInterval(() => {
@@ -82,48 +52,12 @@ const useGame = () => {
     }, 1000);
   };
 
-  const flingOutOfBoard = (rowIndex: number, cellIndex: number, movingCell: number[][]) => {
-    for (let i = 0; i < movingCell.length; i++) {
-      const extendRowIndex = movingCell[i][0] + rowIndex;
-      const extendCellIndex = movingCell[i][1] + cellIndex;
-
-      if (extendCellIndex < 0 || extendCellIndex > 9 || extendRowIndex < 0 || extendRowIndex > 19) {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  const checkUnderCell = (movingCell: number[][]) => {
-    for (let i = 0; i < movingCell.length; i++) {
-      if (board[movingCell[i][0] + 1][movingCell[i][1]] === 2) {
-        return true;
-      }
-    }
-
-    return false;
-  };
-
-  const stopCell = (movingCell: number[][]) => {
-    const newBoard = structuredClone(board);
-    const newMovingCell = structuredClone(movingCell);
-    newMovingCell.map((row: number[]) => {
-      if (row[0] + 1 > 19 || newBoard[row[0] + 1][row[1]] === 2) {
-        newMovingCell.map((row: number[]) => {
-          newBoard[row[0]][row[1]] = 2;
-        });
-      }
-    });
-    return newBoard;
-  };
-
   const keyHandler = (e) => {
     const keyValue = e.code;
 
     if (keyValue === 'ArrowDown') {
       const movingCell = findMovingCell(board);
-      const newBoard = stopCell(movingCell);
+      const newBoard = stopCell(movingCell, board);
       if (newBoard.flat().some((cell) => cell === 2)) {
         setBoard(newBoard);
       }
@@ -151,7 +85,7 @@ const useGame = () => {
       movingCell.map((row) => {
         newBoard[row[0]][row[1]] = 0;
       });
-      while (!flingOutOfBoard(1, 0, movingCell) && !checkUnderCell(movingCell)) {
+      while (!flingOutOfBoard(1, 0, movingCell) && !checkUnderCell(movingCell, board)) {
         movingCell.map((row: number[]) => {
           row[0] += 1;
         });
---
+++ src/functions/functions.tsx
@@ -0,0 +1,73 @@
+const findMovingCell = (board: number[][]) => {
+  const movingCell: number[][] = [];
+
+  board.map((row: number[], rowIndex: number) => {
+    row.map((cell, cellIndex: number) => {
+      if (cell === 1) {
+        movingCell.push([rowIndex, cellIndex]);
+      }
+    });
+  });
+
+  return movingCell;
+};
+
+const moveCell = (
+  rowIndex: number,
+  cellIndex: number,
+  movingCell: number[][],
+  board: number[][],
+) => {
+  const newBoard = structuredClone(board);
+  console.log(rowIndex);
+  movingCell.map(([row, cell]: number[]) => {
+    newBoard[row][cell] = 0;
+  });
+
+  movingCell.map(([row, cell]: number[]) => {
+    const extendRowIndex = row + rowIndex;
+    const extendCellIndex = cell + cellIndex;
+
+    newBoard[extendRowIndex][extendCellIndex] = 1;
+  });
+
+  return newBoard;
+};
+
+const flingOutOfBoard = (rowIndex: number, cellIndex: number, movingCell: number[][]) => {
+  for (let i = 0; i < movingCell.length; i++) {
+    const extendRowIndex = movingCell[i][0] + rowIndex;
+    const extendCellIndex = movingCell[i][1] + cellIndex;
+
+    if (extendCellIndex < 0 || extendCellIndex > 9 || extendRowIndex < 0 || extendRowIndex > 19) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const checkUnderCell = (movingCell: number[][], board: number[][]) => {
+  for (let i = 0; i < movingCell.length; i++) {
+    if (board[movingCell[i][0] + 1][movingCell[i][1]] === 2) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+const stopCell = (movingCell: number[][], board: number[][]) => {
+  const newBoard = structuredClone(board);
+  const newMovingCell = structuredClone(movingCell);
+  newMovingCell.map((row: number[]) => {
+    if (row[0] + 1 > 19 || newBoard[row[0] + 1][row[1]] === 2) {
+      newMovingCell.map((row: number[]) => {
+        newBoard[row[0]][row[1]] = 2;
+      });
+    }
+  });
+  return newBoard;
+};
+
+export { findMovingCell, moveCell, flingOutOfBoard, checkUnderCell, stopCell };
